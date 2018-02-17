e66ef7f2e76f3be287d9938742360b7b623882fc
Alex Shelley <ashelley@purdue.edu>
2018-02-17 17:49:47 -0500

    It now successfully reads back the data written to the database.

--- Back End/sqlite_experiments.js
+++ Back End/sqlite_experiments.js
@@ -4,11 +4,16 @@ initialize_database();
 
 async function initialize_database()
 {
-	let dbPromise = sqlite.open('./experiment_database.sqlite', {Promise});
+	let dbPromise = sqlite.open('./database_contents/experiment_database.sqlite', {Promise});
 	const db = await dbPromise;
 
 	// Create the table
-	let result = await db.get("CREATE TABLE player_scores (playerid int, score int);");
-	
-	// TODO: Read from the table
+	await db.run("CREATE TABLE player_scores (playerid int, score int);");
+	await db.run("INSERT INTO player_scores (playerid, score) VALUES (0, 100);");
+
+	// Read data from the table
+	let result = await db.get("SELECT * FROM player_scores;");
+
+	// Print the read data
+	console.log(result);
 }
