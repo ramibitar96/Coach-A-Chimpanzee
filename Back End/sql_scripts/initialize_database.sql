aa4c037f04c7c690021c180c69695546315b0954
Alex Shelley <ashelley@purdue.edu>
2018-02-17 20:40:32 -0500

    Added cached_rating_total table.  Also added upvote_or_downvote column to coach_ratings.

--- Back End/sql_scripts/initialize_database.sql
+++ Back End/sql_scripts/initialize_database.sql
@@ -12,6 +12,15 @@ CREATE TABLE IF NOT EXISTS coach_ratings
 (
 	student_user_id int,
 	coach_user_id int,
+	upvote_or_downvote boolean,		-- If it's true, it's an upvote.  Else, it's a downvote.
 	FOREIGN KEY(student_user_id) REFERENCES user(user_id),
 	FOREIGN KEY(coach_user_id) REFERENCES user(user_id)
 );
+
+-- Every so often, this table will need to be updated using the above table.
+CREATE TABLE IF NOT EXISTS cached_rating_total
+(
+	coach_user_id int PRIMARY KEY,
+	total_rating int
+);
+
