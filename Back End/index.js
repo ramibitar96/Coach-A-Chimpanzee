335bc3c5adadb29b194615813779f50ddb32a2c0
Alex Shelley <ashelley@purdue.edu>
2018-02-17 19:38:49 -0500

    Moved initialize_database into index.js.  Don't worry, I'll move it to a separate file once I learn how.

--- Back End/index.js
+++ Back End/index.js
@@ -1,13 +1,52 @@
+// Imports
+import sqlite from 'sqlite';
+import fs from 'fs';						// Used by execute_sql_script
+import strip from 'sql-strip-comments';		// Used by execute_sql_script
+
 var express = require('express');
-var app = express();
+
 
 // Whenever someone requests any file, we respond with "Hello World"
+var app = express();
 app.get('/', function (req, res)
 {
 	res.send('Hello World!');
 });
 
+// Start handling requests
 app.listen(3000, function()
 {
 	console.log('Listening on port 3000!');
 });
+
+
+// TODO: move these functions off to a separate file.
+// will do this once I learn how.
+
+// Initializes the database, creating all the tables and stuff.
+async function initialize_database()
+{
+	let dbPromise = sqlite.open('./database_contents/experiment_database.sqlite', {Promise});
+	const db = await dbPromise;
+
+	// Create the table
+	await execute_sql_script("./sql_scripts/initialize_database.sql", db);
+
+	// Read data from the table
+	let result = await db.get("SELECT * FROM player_scores;");
+
+	// Print the read data
+	console.log(result);
+}
+
+// Executes the SQL script specified by filePath.
+// Returns a Promise<sqlite.Statement> when it's done.
+// filePath's type is string.  db's type is sqlite.Database
+async function execute_sql_script(filePath, db)		
+{
+	let sqlText = fs.readFileSync(filePath, 'utf8');	// Read the SQL file into a string
+	let noComments = strip(sqlText);					// Remove all comments so sqlite can execute it.
+
+	// Execute it.
+	return db.exec(noComments);
+}
