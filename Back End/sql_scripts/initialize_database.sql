3409725fb60757ac7cfb082a19e2c628aa90604d
Alex Shelley <ashelley@purdue.edu>
2018-02-17 19:27:42 -0500

    Wrote execute_sql_script.

---
+++ Back End/sql_scripts/initialize_database.sql
@@ -0,0 +1,3 @@
+-- Will this comment get stripped?
+CREATE TABLE if not exists player_scores (playerid int, score int);
+INSERT INTO player_scores (playerid, score) VALUES (0, 100);
--- Back End/sqlite_experiments.js
+++ Back End/sqlite_experiments.js
@@ -1,4 +1,6 @@
 import sqlite from 'sqlite';
+import fs from 'fs';
+import strip from 'sql-strip-comments';
 
 initialize_database();
 
@@ -8,12 +10,23 @@ async function initialize_database()
 	const db = await dbPromise;
 
 	// Create the table
-	await db.run("CREATE TABLE player_scores (playerid int, score int);");
-	await db.run("INSERT INTO player_scores (playerid, score) VALUES (0, 100);");
+	await execute_sql_script("./sql_scripts/initialize_database.sql", db);
 
 	// Read data from the table
 	let result = await db.get("SELECT * FROM player_scores;");
 
 	// Print the read data
 	console.log(result);
+}
+
+// Executes the SQL script specified by filePath.
+// Returns a Promise<sqlite.Statement> when it's done.
+// filePath's type is string.  db's type is sqlite.Database
+async function execute_sql_script(filePath, db)		
+{
+	let sqlText = fs.readFileSync(filePath, 'utf8');	// Read the SQL file into a string
+	let noComments = strip(sqlText);					// Remove all comments so sqlite can execute it.
+
+	// Execute it.
+	return db.exec(noComments);
 }
