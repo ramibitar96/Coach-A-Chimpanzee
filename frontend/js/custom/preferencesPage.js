796622c2e47fef80c8f18c139763969b7d16bd75
james shao <jshao100@gmail.com>
2018-02-23 13:52:50 -0500

    hardcoded preferences parsing for json

--- frontend/js/custom/preferencesPage.js
+++ frontend/js/custom/preferencesPage.js
@@ -1,38 +1,55 @@
-$('.pref-load').on('load', function() {
+//$('.pref-load').on('load', function() {
 
-            var hardcode = '{' + 
+var hardcode = ' { "error_code": 200, "user": {"summoner_name": "ImDaBest","current_rank": 4},"student":{"last_hitting": true,"macro": false,"map_awareness": false,"skillshots": true,"freezing_waves": false,"matchups": false,"setup": true,"min_coach_rank": 5},"coach":{"last_hitting": true,"macro": false, "map_awareness": false, "skillshots": true, "freezing_waves": true,"matchups": true, "setup": false, "max_student_rank": 3 }}';
 
-            "error_code": 200,
+var json = JSON.parse(hardcode);
+var json_user = json["user"];
+var json_student = json["student"];
+var json_coach = json["coach"];
 
-            "user":
-            {
-                "summoner_name": "ImDaBest",
-                "current_rank": 4
-            },
+var error = json["error_code"]
 
-            "student":
-            {
-                "last_hitting": true,
-                "macro": false,
-                "map_awareness": false,
-                "skillshots": true,
-                "freezing_waves": false,
-                "matchups": false,
-                "setup": true,
-                "min_coach_rank": 5
-            },
+//set name
+$("#SummonerName").val(json_user["summoner_name"]);
 
-            "coach":
-            {
-                "last_hitting": true,
-                "macro": false,
-                "map_awareness": false,
-                "skillshots": true,
-                "freezing_waves": true,
-                "matchups": true,
-                "setup": false,
-                "max_student_rank": 3
-            }
-    + '}';
+//set rank
+$("#rank").val(json_user["current_rank"]);
 
-});
+//set student preferences
+if (json_student["last_hitting"] && json_student["macro"] && json_student["map_awareness"] &&
+	json_student["skillshots"] && json_student["freezing_waves"] && json_student["matchups"] &&
+	json_student["setup"]) {
+	//all true, set value to everything
+	$("#Everything").prop("checked", true);	
+	} else {
+		$("#LastHitting").prop("checked", json_student["last_hitting"]);
+		$("#Macro").prop("checked", json_student["macro"]);
+		$("#MapAwareness").prop("checked", json_student["map_awareness"]);
+		$("#Skillshots").prop("checked", json_student["skillshots"]);
+		$("#Freezing").prop("checked", json_student["freezing_waves"]);
+		$("#Matchups").prop("checked", json_student["matchups"]);
+		$("#SetUp").prop("checked", json_student["setup"]);
+}	
+$("#min_rank").val(json_student["min_coach_rank"]);
+
+//set coach preferences
+if (json_coach["last_hitting"] && json_coach["macro"] && json_coach["map_awareness"] &&
+	json_coach["skillshots"] && json_coach["freezing_waves"] && json_coach["matchups"] &&
+	json_coach["setup"]) {
+	//all true, set value to everything
+	$("#EverythingC").prop("checked", true);	
+	} else {
+		$("#LastHittingC").prop("checked", json_coach["last_hitting"]);
+		$("#MacroC").prop("checked", json_coach["macro"]);
+		$("#MapAwarenessC").prop("checked", json_coach["map_awareness"]);
+		$("#SkillshotsC").prop("checked", json_coach["skillshots"]);
+		$("#FreezingC").prop("checked", json_coach["freezing_waves"]);
+		$("#MatchupsC").prop("checked", json_coach["matchups"]);
+		$("#SetUpC").prop("checked", json_coach["setup"]);
+}	
+$("#maxStudentRank").val(json_coach["max_student_rank"]);
+
+
+
+
+//});
