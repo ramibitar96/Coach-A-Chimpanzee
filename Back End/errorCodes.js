7c80ee503cea768fff4f35b2df22cf83af11939e
Alex Shelley <ashelley@purdue.edu>
2018-02-22 19:51:44 -0500

    Added more error codes.

--- Back End/errorCodes.js
+++ Back End/errorCodes.js
@@ -4,7 +4,9 @@ const ErrorCodeEnum =
     USERNAME_EXISTS: 1,
     EMAIL_INVALID: 2,
     BAD_JSON_OBJECT: 3,
-    WRONG_PASSWORD: 4
+    WRONG_PASSWORD: 4,
+    TOKEN_EXPIRED: 5,
+    TOKEN_INVALID: 6
 };
 
 module.exports = ErrorCodeEnum;
