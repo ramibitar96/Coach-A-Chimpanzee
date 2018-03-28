841e2f3cd9e3efa9e232fa6c7dc308ccdee2d51d
james shao <jshao100@gmail.com>
2018-03-28 16:12:03 -0400

    redirect fixed, check if logged in and redirect, logout redirect complete

--- frontend/js/custom/signup.js
+++ frontend/js/custom/signup.js
@@ -1,14 +1,18 @@
 //signup pass to ajax
-$('.signup-form').on('submit', function() {
+$('.signup-form').on('submit', function(e) {
+	e.preventDefault();
 	var data = $('.signup-form').serializeArray().reduce(function(obj, item) {
 		obj[item.name] = item.value;
 		return obj;
 	}, {});
 
+	var user = data['username'];
+	var pass = data['password'];
+
 	var body = 
 		'{' +
-		'"username":' + '"' + data['username'] + '",' +
-		'"password":' + '"' + data['password'] + '",' +
+		'"username":' + '"' + user + '",' +
+		'"password":' + '"' + pass + '",' +
 		'"email":' + '"' + data['email'] + '",' +
 		'"summoner_id": 1' +
 		'}';
@@ -20,8 +24,42 @@ $('.signup-form').on('submit', function() {
 			processData: false,
 			async: false,
 			data: body,
-			success: function (data){
-				alert(data["error_code"]); 
+			success: function(data) {
+				var error = data["error_code"];
+				if (error == 0) {
+					var body = 
+						'{' +
+						'"username":' + '"' + user + '",' +
+						'"password":' + '"' + pass + '"' +
+						'}';
+					login(body);
+				}
+				else if (error == 1) {
+					alert("User already exists");
+				}
+				else if (error == 2) {
+					alert("Email invalid");
+				}
+				else if (error == 3) {
+					alert("Incomplete fields");
+				}
 			}
 	});
 });
+
+function login(body) {
+	$.ajax({
+			type: "POST",
+			url: "http://localhost:3000/login",
+			contentType: 'application/json',
+			processData: false,
+			async: false,
+			data: body,
+			success: function (data){
+				var response = data;
+				if (response["error_code"] == 0) {
+					window.location.assign("queue.html");
+				}
+			}
+	});
+}
