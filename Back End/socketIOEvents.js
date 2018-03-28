8330ba46a73154dd60a020006831ec0b546c1f80
Alex Shelley <ashelley@purdue.edu>
2018-03-27 21:19:43 -0400

    It now identifies the user that connected to it by their session token.

--- Back End/socketIOEvents.js
+++ Back End/socketIOEvents.js
@@ -3,6 +3,7 @@ module.exports = function(io)
     // Called whenever a client connects to socket.io
     io.on('connection', function(socket)
     {
-        console.log("A user connected");
+        console.log("A user connected, bearing the cookie " + socket.handshake.headers.cookie);
     });
+
 }
