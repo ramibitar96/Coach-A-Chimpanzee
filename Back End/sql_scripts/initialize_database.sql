ce2ecaa58c33b0a14158f24b9913e4a8b76ac4f7
Alex Shelley <ashelley@purdue.edu>
2018-03-28 18:10:35 -0400

    Removed coaching session table, renamed ratings table to reviews.  Added review text and timestamp.

--- Back End/sql_scripts/initialize_database.sql
+++ Back End/sql_scripts/initialize_database.sql
@@ -8,11 +8,15 @@ CREATE TABLE IF NOT EXISTS user
 	password_salt varbinary
 );
 
-CREATE TABLE IF NOT EXISTS coach_ratings
+CREATE TABLE IF NOT EXISTS coach_reviews
 (
 	student_user_id int,
 	coach_user_id int,
+	review_date datetime,
+
 	upvote_or_downvote boolean,		-- If it's true, it's an upvote.  Else, it's a downvote.
+	review_text varchar,
+
 	FOREIGN KEY(student_user_id) REFERENCES user(user_id),
 	FOREIGN KEY(coach_user_id) REFERENCES user(user_id)
 );
@@ -23,16 +27,6 @@ CREATE TABLE IF NOT EXISTS cached_rating_total
 	coach_user_id int PRIMARY KEY,
 	total_rating int,
 	FOREIGN KEY(coach_user_id) REFERENCES user(user_id)
-);
-
-CREATE TABLE IF NOT EXISTS coaching_session
-(
-	session_id int PRIMARY KEY,
-	student_user_id int,
-	coach_user_id int,
-	time_stamp int,
-	FOREIGN KEY(student_user_id) REFERENCES user(rowid),
-	FOREIGN KEY(coach_user_id) REFERENCES user(rowid)
 );
 
 CREATE TABLE IF NOT EXISTS coachee_skills
