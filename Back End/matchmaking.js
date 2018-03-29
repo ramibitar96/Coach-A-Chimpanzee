c7251441501944ed8481f0967b0f60e4a8bcd67d
Rohan Swaroop <nahor.swaroop@gmail.com>
2018-03-28 20:10:10 -0400

    Temporary commit to sync with master

--- Back End/matchmaking.js
+++ Back End/matchmaking.js
@@ -1,3 +1,8 @@
+// Imports
+const authUtils = require('./authenticationUtils.js');
+const ErrorCodeEnum = require('./errorCodes.js');
+const cookieParser = require('cookie');
+
 var Rank = {
 	Any: 0,
 	Bronze: 1,
@@ -153,7 +158,90 @@ module.exports = {
 	findPartner
 }
 
-test1();
+let userSockets = {};   // A dictionary mapping usernames to their sockets
+let socketUsers = {};   // A dictionary mapping sockets to their usernames
+
+
+module.exports = function(io)
+{
+    // Called whenever a client connects to socket.io
+    // Associates the socket with the user
+    io.on('connection', async function(socket)
+    {
+        // Check the session token to find out what user this is
+        let cookie = cookieParser.parse(socket.handshake.headers.cookie);
+        let session_token = cookie.session_token;
+
+        let authResult = await authUtils.checkToken(session_token);
+
+        // Don't do anything if the cookie failed
+        // TODO: Send some kind of error message 
+        if (authResult.error_code != ErrorCodeEnum.SUCCESS)
+        {
+            console.log("chatroom: error trying to authenticate token " + session_token + "\n error code " + authResult.error_code);
+            return;
+        }
+
+        let username = authResult.username;
+
+        // Associate the socket with the username
+        userSockets[username] = socket;
+        socketUsers[socket.id] = authResult.username;
+        console.log("associated socket " + socket.id + " with username " + authResult.username);
+
+        // When a user disconnects, remove them from both dictionaries and student/coach arrays
+        socket.on('disconnect', function()
+        {
+            let username = socketUsers[socket.id];
+            delete userSockets[username];
+            delete socketUsers[socket.id];
+
+            for (var i = 0; i < students.length; i++) {
+            	if (students[i].name == username) {
+            		students.splice(i, 1);
+            		console.log("Student " + username + " removed from queue.");
+            		break;
+            	}
+            }
+
+            for (var i = 0; i < coaches.length; i++) {
+            	if (coaches[i].name == username) {
+            		coaches.splice(i, 1);
+            		console.log("Coach " + username + " removed from queue.");
+            		break;
+            	}
+            }
+        });
+
+        // When a user sends a message, forward it to their partner.
+        socket.on('queueType', function(msg)
+        {
+            console.log(authResult.username + ": " + msg);
+
+            // 0 = student, 1 = coach
+            console.log("QUEUE TYPE: " + msg);
+
+            /*let partnerName = findPartner(username);
+            let userSocket = userSockets[username];
+            let partnerSocket = userSockets[partnerName];
+
+            let userObj =
+            {
+                partner: partnerName,
+            };
+
+            let partnerObj =
+            {
+                partner: username
+            };
+
+            userSocket.emit('partner_found', partnerObj);
+            partnerSocket.emit('partner_found', userObj);*/
+        });
+    });
+}
+
+//test1();
 
 // Test cases
 function test1() {
