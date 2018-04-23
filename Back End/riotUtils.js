ca5a08807fcc909185438d134b1c0c758a399083
Alex Shelley <ashelley@purdue.edu>
2018-04-23 18:46:13 -0400

    Implemented get_rank

--- Back End/riotUtils.js
+++ Back End/riotUtils.js
@@ -4,7 +4,8 @@ const request = require('request-promise-native');
 const API_KEY = "RGAPI-f11b77d1-a7c9-4e32-88b9-2f874d1fd29c";
 
 /**
- * Returns the summonder id of the given user
+ * Returns the summoner data of the given user
+ * output matches that of /lol/summoner/v3/summoners/by-name/{summonerName}
  * @param {*} username 
  */
 async function get_summoner_data(username)
@@ -28,15 +29,39 @@ async function get_summoner_data(username)
 }
 
 /**
- * Returns the rank of the given user
+ * Returns the given user's solo queue rank
+ * If they don't have a solo queue rank, returns null
  * @param {*} username 
  */
-function get_rank(username)
+async function get_rank(username)
 {
     // TODO: Cache the result in the database
 
-    // Get
-    let url = "";
+    let summoner_data = await get_summoner_data(username);
+
+    // Query Riot's servers
+    let url = "https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/" + summoner_data.id;
+    let options =
+    {
+        headers:
+        {
+            'X-Riot-Token': API_KEY
+        },
+
+        json: true
+    };
+
+    let results = await request.get(url, options);
+
+    // Search for and return their solo queue rank
+    for (let leagueInfo in results)
+    {
+        if (leagueInfo.queueType == "RANKED_SOLO_5x5")
+            return leagueInfo.tier;
+    }
+
+    // We couldn't find their solo queue rank, so return null
+    return null;
 }
 
 
